1c67ff4ed46c896fc2ecf46b5cf96b6067bbadb2
Levina Tani <edelaida@gmail.com>
2024-04-23 18:06:08 +0300

    phonebook

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import css from "./App.module.css";
+
+function App() {
+  return (
+    <div className={css.block}>
+      <h1>Phonebook</h1>
+      {/* <ContactForm />
+      <SearchBox />
+      <ContactList /> */}
+    </div>
+  );
+}
+
+export default App;
